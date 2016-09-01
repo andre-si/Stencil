92a84a7c73f244d28ad07ca938c7cf7e1d205d61
Jamie White <jamie@skim.it>
2016-09-01 11:31:58 +0100

    Adding an xcode project

---
+++ Stencil.h
@@ -0,0 +1,19 @@
+//
+//  Stencil.h
+//  Stencil
+//
+//  Created by James White on 01/09/2016.
+//  Copyright © 2016 Kyle Fuller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stencil.
+FOUNDATION_EXPORT double StencilVersionNumber;
+
+//! Project version string for Stencil.
+FOUNDATION_EXPORT const unsigned char StencilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stencil/PublicHeader.h>
+
+
